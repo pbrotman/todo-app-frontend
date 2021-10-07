3153f16548c0821df022a22aefcd9099f99ea2e3
PARKER BROTMAN <pb1925a@student.american.edu>
2021-10-07 15:20:16 -0400

    Updated todo-data service to talk to JPA API

--- src/app/service/data/todo-data.service.ts
+++ src/app/service/data/todo-data.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { API_URL } from 'src/app/app.constants';
+import { API_URL, TODO_JPA_API_URL } from 'src/app/app.constants';
 import { Todo } from 'src/app/list-todos/list-todos.component';
 
 @Injectable({
@@ -13,24 +13,24 @@ export class TodoDataService {
   ) { }
 
   retrieveAllTodos(username : string){
-    return this.http.get<Todo[]>(`${API_URL}/users/${username}/todos/`)
+    return this.http.get<Todo[]>(`${TODO_JPA_API_URL}/users/${username}/todos/`)
   }
 
   deleteTodo(username : string, id : number){
-    return this.http.delete(`${API_URL}/users/${username}/todos/${id}`)
+    return this.http.delete(`${TODO_JPA_API_URL}/users/${username}/todos/${id}`)
   }
 
   retrieveTodo(username : string, id : number){
-    return this.http.get<Todo>(`${API_URL}/users/${username}/todos/${id}`)
+    return this.http.get<Todo>(`${TODO_JPA_API_URL}/users/${username}/todos/${id}`)
   }
 
   updateTodo(username : string, id : number, todo: Todo){
     // console.log(todo)
-    return this.http.put(`${API_URL}/users/${username}/todos/${id}`, todo)
+    return this.http.put(`${TODO_JPA_API_URL}/users/${username}/todos/${id}`, todo)
   }
 
   createTodo(username : string, todo: Todo){
     console.log(`todo-data.service createTodo: id=${todo.id}`)
-    return this.http.post(`${API_URL}/users/${username}/todos`, todo)
+    return this.http.post(`${TODO_JPA_API_URL}/users/${username}/todos`, todo)
   }
 }
